99be921aa72bc9dc78ef36ab668e413172733899
RBxRobson <robsonnbarbosa0@gmail.com>
2024-11-22 16:04:16 -0300

    fix: Ajusta o nome do app

--- posting/apps.py
+++ posting/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class PostingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'postings'
+    name = 'posting'
 
     def ready(self):
         import posting.signals
