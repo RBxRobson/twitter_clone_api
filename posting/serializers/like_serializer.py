0675001f921276a112336bfab27d898fcafeb290
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-03 15:41:33 -0300

    refactor: Refatora serializer do model Like

--- posting/serializers/like_serializer.py
+++ posting/serializers/like_serializer.py
@@ -1,26 +1,33 @@
 from rest_framework import serializers
-from posting.models import Like, Post
-from accounts.models import User
+from posting.models import Like, Post, Comment
 
 class LikeSerializer(serializers.ModelSerializer):
-    # Relacionamento com o usuário
-    user = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
-    
-    # Relacionamento com o post
-    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
-    
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all(), required=False)
+    comment = serializers.PrimaryKeyRelatedField(queryset=Comment.objects.all(), required=False)
+
     class Meta:
         model = Like
-        fields = ['id', 'user', 'post', 'created_at']
-        read_only_fields = ['created_at']
+        fields = ['id', 'user', 'post', 'comment', 'created_at']
+        read_only_fields = ['id', 'user', 'created_at']
 
     def validate(self, data):
-        # Verifica se o usuário já curtiu o post
-        if Like.objects.filter(user=data['user'], post=data['post']).exists():
-            raise serializers.ValidationError("Você já curtiu o post.")
+        # Garante que o like está associado a um post OU a um comentário, mas não a ambos
+        if not data.get('post') and not data.get('comment'):
+            raise serializers.ValidationError("Um like deve estar associado a um post ou a um comentário.")
+        if data.get('post') and data.get('comment'):
+            raise serializers.ValidationError("Um like não pode estar associado a um post e a um comentário simultaneamente.")
         return data
 
-    def create(self, validated_data):
-        # Cria a curtir (like) se a validação passar
-        like = Like.objects.create(**validated_data)
-        return like
+    #Customiza a resposta com base no tipo de curtida.
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Remove o campo post caso seja uma curtida em comentário
+        if instance.comment:
+            representation.pop('post', None)
+        # Remove o campo comment caso seja uma curtida em post
+        else:
+            representation.pop('comment', None)
+
+        return representation
