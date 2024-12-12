77599220939a9ece9040350c9715c31a88533a6e
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-12 19:09:18 -0300

    feat: Ajusta a factory do model User

--- accounts/factories/user_factory.py
+++ accounts/factories/user_factory.py
@@ -1,18 +1,14 @@
 import factory
 from accounts.models import User
-from django.contrib.auth.hashers import make_password
+from faker import Faker
+
+fake = Faker()
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
-        skip_postgeneration_save = True
 
+    # Garante que o email será único
+    email = factory.LazyAttribute(lambda _: fake.unique.email())  
     name = factory.Faker('name')
-    email = factory.Faker('email')
-    password = factory.LazyFunction(lambda: make_password('Password123'))
-    is_active = True
-
-    # Override para salvar após a criação dos campos gerados
-    @classmethod
-    def _after_postgeneration(cls, obj, create, results=None):
-        obj.save()
+    password = factory.PostGenerationMethodCall('set_password', 'Password123')
