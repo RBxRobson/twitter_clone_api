a9f71944bdb6317ac8292bdbeb1fad21538ed981
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-23 17:32:26 -0300

    feat: Adiciona url inicial para exibir o README.md da api

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("postings/", include("posting.urls")),
+    path("", views.read_readme, name="home")
 ]
