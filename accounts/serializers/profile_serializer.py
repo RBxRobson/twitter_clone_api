08e34f1a04e2270026ea64ec743e241f44e7b3ef
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-26 10:27:42 -0300

    chore: Melhora retorno das urls de mídia

--- accounts/serializers/profile_serializer.py
+++ accounts/serializers/profile_serializer.py
@@ -5,3 +5,15 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['bio', 'avatar', 'header']
+
+    def get_avatar(self, obj):
+        request = self.context.get('request')
+        if obj.avatar and request:
+            return request.build_absolute_uri(obj.avatar.url)
+        return None
+
+    def get_header(self, obj):
+        request = self.context.get('request')
+        if obj.header and request:
+            return request.build_absolute_uri(obj.header.url)
+        return None
