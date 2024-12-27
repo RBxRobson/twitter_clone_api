09393f8ee7f1d9770436f663c4041b937af229da
RBxRobson <robsonnbarbosa0@gmail.com>
2024-12-27 11:15:46 -0300

    fix: Retira carregamento do arquivo .env para realizar o deploy corretamente no railway

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 
 # Carregar as variáveis de ambiente do arquivo .env
-environ.Env.read_env(BASE_DIR / ".env")
+# environ.Env.read_env(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
