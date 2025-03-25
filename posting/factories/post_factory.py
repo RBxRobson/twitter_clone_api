ae4ff7de78c1c6e200c9d9c3e568614349bca73a
RBxRobson <robsonnbarbosa0@gmail.com>
2025-03-25 13:28:23 -0300

    refactor: Ajuste na factory do model Post

--- posting/factories/post_factory.py
+++ posting/factories/post_factory.py
@@ -1,23 +1,14 @@
 import factory
-from factory.django import DjangoModelFactory
-from accounts.factories import UserFactory
+from faker import Faker
 from posting.models import Post
 
+faker = Faker()
 
-class PostFactory(DjangoModelFactory):
+class PostFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Post
-        skip_postgeneration_save = True
 
-    user = factory.SubFactory(UserFactory)
-    post_type = factory.Iterator([Post.ORIGINAL, Post.REPOST, Post.QUOTE, Post.COMMENT])
-    content = factory.LazyAttribute(lambda obj: factory.Faker("text", max_nb_chars=280).generate({}) if obj.post_type != Post.REPOST else "")
-
-    @factory.post_generation
-    def original_post(self, create, extracted, **kwargs):
-        if not create:
-            return
-
-        if self.post_type in {Post.REPOST, Post.QUOTE, Post.COMMENT}:
-            self.original_post = extracted if extracted else PostFactory(post_type=Post.ORIGINAL)
-            self.save()
+    user = factory.SubFactory("core.factories.UserFactory")
+    content = factory.LazyAttribute(lambda o: "" if o.post_type == Post.REPOST else faker.text(max_nb_chars=280))
+    post_type = Post.ORIGINAL
+    original_post = None
