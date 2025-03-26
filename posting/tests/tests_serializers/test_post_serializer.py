af1fd0e51054b61e4e26386c93e8b6f09601cde3
RBxRobson <robsonnbarbosa0@gmail.com>
2025-03-26 09:18:15 -0300

    refactor: Ajuste nos testes do serializer do model Post

--- posting/tests/tests_serializers/test_post_serializer.py
+++ posting/tests/tests_serializers/test_post_serializer.py
@@ -1,65 +1,54 @@
 import pytest
+from rest_framework.test import APIRequestFactory
 from posting.models import Post
 from posting.factories import PostFactory
 from posting.serializers import PostSerializer
 from posting.utils import create_post
 
+@pytest.fixture
+def api_request_factory():
+    return APIRequestFactory()
 
-# Testa se uma postagem original está sendo criada corretamente
 @pytest.mark.django_db
-def test_post_original_creation():
+def test_post_original_creation(api_request_factory):
     post, user = create_post()
-
-    # Instancia o serializer passando os dados do post
-    serializer = PostSerializer(post)
-
-    # Verifica os dados serializados
+    request = api_request_factory.get("/fake-url/")
+    request.user = user
+    
+    serializer = PostSerializer(post, context={"request": request})
+    
     assert serializer.data
     assert serializer.data["post_type"] == Post.ORIGINAL
-    assert serializer.data["user"] == user.id
+    assert serializer.data["user_details"]["id"] == user.id
     assert serializer.data["content"] == post.content
-
-    # Verificação da existência no banco de dados
     assert Post.objects.filter(id=post.id).exists()
 
-
-# Testa se uma repostagem está sendo criada corretamente
 @pytest.mark.django_db
-def test_post_repost_creation():
+def test_post_repost_creation(api_request_factory):
     post, user = create_post()
-
-    # Cria um repost usando o post original criado anteriormente
     repost = PostFactory.create(user=user, post_type=Post.REPOST, original_post=post)
-
-    # Instancia o serializer passando os dados do post
-    serializer = PostSerializer(repost)
-
-    # Verifica os dados serializados
+    request = api_request_factory.get("/fake-url/")
+    request.user = user
+    
+    serializer = PostSerializer(repost, context={"request": request})
+    
     assert serializer.data
     assert serializer.data["post_type"] == Post.REPOST
-    assert serializer.data["user"] == user.id
+    assert serializer.data["user_details"]["id"] == user.id
     assert serializer.data["content"] == ""
-
-    # Verificação da existência no banco de dados
     assert Post.objects.filter(id=repost.id).exists()
 
-
-# Testa se uma citação está sendo criada corretamente
 @pytest.mark.django_db
-def test_post_quote_creation():
+def test_post_quote_creation(api_request_factory):
     post, user = create_post()
-
-    # Cria uma citação usando o post original criado anteriormente
     quote = PostFactory.create(user=user, post_type=Post.QUOTE, original_post=post)
-
-    # Instancia o serializer passando os dados do post
-    serializer = PostSerializer(quote)
-
-    # Verifica os dados serializados
+    request = api_request_factory.get("/fake-url/")
+    request.user = user
+    
+    serializer = PostSerializer(quote, context={"request": request})
+    
     assert serializer.data
     assert serializer.data["post_type"] == Post.QUOTE
-    assert serializer.data["user"] == user.id
+    assert serializer.data["user_details"]["id"] == user.id
     assert serializer.data["content"] == quote.content
-
-    # Verificação da existência no banco de dados
     assert Post.objects.filter(id=quote.id).exists()
